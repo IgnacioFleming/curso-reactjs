378fd5b1d7f1d4a1bf13ccde8691698d027c6bbb
Ignacio Fleming <ignacioflemings@gmail.com>
2023-04-10 23:33:41 -0300

    "hago responsivo el AppBar"

--- src/components/Nabvar/CategoriesDesktop.jsx
+++ src/components/Nabvar/CategoriesDesktop.jsx
@@ -1,24 +1,27 @@
-import { Box, Grid, Typography } from "@mui/material";
-
+import { Grid, Typography } from "@mui/material";
 import React from "react";
-
+const alignment = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
 export const CategoriesDesktop = () => {
   return (
     <Grid container sx={{ justifyContent: "center" }}>
-      <Grid item xs={2}>
-        <Typography>Todos</Typography>
+      <Grid item xs={2} sx={alignment}>
+        <Typography align="center">Todos</Typography>
       </Grid>
-      <Grid item xs={2}>
-        <Typography>Computación</Typography>
+      <Grid item xs={2} sx={alignment}>
+        <Typography align="center">Computación</Typography>
       </Grid>
-      <Grid item xs={2}>
-        <Typography>Muebles</Typography>
+      <Grid item xs={2} sx={alignment}>
+        <Typography align="center">Muebles</Typography>
       </Grid>
-      <Grid item xs={2}>
-        <Typography>Sistemas de Monitoreo</Typography>
+      <Grid item xs={2} sx={alignment}>
+        <Typography align="center">Sistemas de Monitoreo</Typography>
       </Grid>
-      <Grid item xs={2}>
-        <Typography>Equipamiento</Typography>
+      <Grid item xs={2} sx={alignment}>
+        <Typography align="center">Equipamiento</Typography>
       </Grid>
     </Grid>
   );
--- src/components/Nabvar/Navbar.jsx
+++ src/components/Nabvar/Navbar.jsx
@@ -1,5 +1,6 @@
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Hidden, Toolbar } from "@mui/material";
 import { CategoriesDesktop } from "./CategoriesDesktop";
+import CartWidget from "../CartWidget/CartWidget";
 
 const Navbar = () => {
   return (
@@ -9,7 +10,10 @@ const Navbar = () => {
           src="https://res.cloudinary.com/dah7yxmc5/image/upload/v1680611686/logo_ecommerce_ci16kw.png"
           alt="Logo de la empresa"
         />
-        <CategoriesDesktop />
+        <Hidden mdDown>
+          <CategoriesDesktop />
+        </Hidden>
+        <CartWidget />
       </Toolbar>
     </AppBar>
   );
