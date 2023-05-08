574ae0910929427db928bc2310a0e42a9d7122b1
Ignacio Fleming <ignacioflemings@gmail.com>
2023-05-08 19:39:35 -0300

    doy estilo al Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,6 +6,8 @@ import {
   CardContent,
   CardMedia,
   Grid,
+  Paper,
+  Stack,
   Typography,
 } from "@mui/material";
 import React from "react";
@@ -13,23 +15,135 @@ import { AiOutlineMinusSquare, AiOutlinePlusSquare } from "react-icons/ai";
 import { RxCounterClockwiseClock } from "react-icons/rx";
 import { Link } from "react-router-dom";
 import useCounter from "../../utils/hooks/useCounter";
+import styled from "@emotion/styled";
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 const Cart = ({ cart, deleteFromCart }) => {
   //   const { counter, agregar, quitar, reset } = useCounter(0);
   return (
-    <div>
-      {cart.map((e) => {
-        console.log(cart);
-        return (
-          <div key={e.id} style={{ border: "solid 1px black" }}>
-            <h1>{e.title}</h1>
-            <h2>{e.quantity}</h2>
-            <Button variant="contained" onClick={() => deleteFromCart(e.id)}>
-              Eliminar
-            </Button>
-          </div>
-        );
-      })}
+    <div style={{ display: "flex", justifyItems: "center" }}>
+      <Stack
+        direction="column"
+        sx={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          marginTop: 10,
+          marginBottom: 10,
+        }}
+        spacing={5}
+      >
+        {cart.map((e) => {
+          console.log(cart);
+          return (
+            <Item sx={{ width: "80%", height: 150 }} elevation={5}>
+              <Grid container>
+                <Grid
+                  item
+                  md={1}
+                  sx={{ display: "flex", alignItems: "center" }}
+                >
+                  <img
+                    style={{
+                      height: "80%",
+                      width: "80%",
+                      objectFit: "contain",
+                    }}
+                    src={e.img}
+                  />
+                </Grid>
+                <Grid
+                  item
+                  md={5}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    gap: 3,
+                  }}
+                >
+                  <Typography gutterBottom mt={5} variant="h5" color="initial">
+                    {e.title}
+                  </Typography>
+                  <Typography
+                    paragraph
+                    variant="subtitle2"
+                    color="initial"
+                    sx={{ width: "90%" }}
+                  >
+                    {e.description}
+                  </Typography>
+                </Grid>
+                <Grid
+                  item
+                  md={2}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    gap: 1,
+                    alignItems: "flex-start",
+                    paddingLeft: 6,
+                  }}
+                >
+                  <Typography
+                    gutterBottom
+                    sx={{ fontWeight: "bold" }}
+                    variant="subtitle1"
+                    color="initial"
+                  >
+                    Cantidad
+                  </Typography>
+                  <Typography gutterBottom variant="h5" color="initial">
+                    {e.quantity}
+                  </Typography>
+                </Grid>
+                <Grid
+                  item
+                  md={2}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "flex-start",
+                    gap: 1,
+                  }}
+                >
+                  <Typography
+                    gutterBottom
+                    sx={{ fontWeight: "bold" }}
+                    variant="subtitle1"
+                    color="initial"
+                  >
+                    Importe
+                  </Typography>
+                  <Typography gutterBottom variant="h5" color="initial">
+                    {e.quantity * e.price}
+                  </Typography>
+                </Grid>
+                <Grid
+                  item
+                  md={2}
+                  sx={{ display: "flex", alignItems: "center" }}
+                >
+                  <Button
+                    variant="contained"
+                    onClick={() => deleteFromCart(e.id)}
+                  >
+                    Eliminar
+                  </Button>
+                </Grid>
+              </Grid>
+            </Item>
+          );
+        })}
+      </Stack>
     </div>
   );
 };
