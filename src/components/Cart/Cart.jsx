c1450f389c73d0ee68844c6fa55a9017af21412f
Ignacio Fleming <ignacioflemings@gmail.com>
2023-05-12 16:48:37 -0300

    termino el modelado del cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,22 +1,16 @@
 import {
   Box,
   Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
   Divider,
   Grid,
+  List,
   Paper,
   Stack,
   Typography,
 } from "@mui/material";
 import React from "react";
-import { AiOutlineMinusSquare, AiOutlinePlusSquare } from "react-icons/ai";
-import { RxCounterClockwiseClock } from "react-icons/rx";
-import { Link } from "react-router-dom";
-import useCounter from "../../utils/hooks/useCounter";
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -25,11 +19,23 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-const Cart = ({ cart, deleteFromCart, totalAmount }) => {
-  //   const { counter, agregar, quitar, reset } = useCounter(0);
+const boxStyle = {
+  paddingTop: 5,
+  paddingBottom: 5,
+  display: "flex",
+  justifyContent: "flex-end",
+  width: "90%",
+};
 
+const Cart = ({ cart, deleteFromCart, totalAmount }) => {
   return (
-    <div style={{ display: "flex", justifyItems: "center" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
       <Stack
         direction="column"
         sx={{
@@ -140,105 +146,28 @@ const Cart = ({ cart, deleteFromCart, totalAmount }) => {
             </Item>
           );
         })}
-
-        <Box sx={{ width: "80%", borderTop: "2px solid #B4B0B0" }}>
+      </Stack>
+      <List sx={{ width: "80%" }}>
+        <Divider variant="fullWidth"></Divider>
+        <Box sx={boxStyle}>
           <Typography variant="h4" color="initial">
             Total : {totalAmount}
           </Typography>
         </Box>
-      </Stack>
+        <Divider variant="fullWidth"></Divider>
+        <Box sx={{ ...boxStyle, gap: 2 }}>
+          <Link to="/">
+            <Button variant="contained">Volver</Button>
+          </Link>
+          <Link to="/checkout">
+            <Button variant="contained" to="/checkout">
+              Terminar Compra
+            </Button>
+          </Link>
+        </Box>
+      </List>
     </div>
   );
 };
 
 export default Cart;
-
-//     <div style={{ display: "flex", justifyContent: "center" }} key={item.id}>
-//       <Card
-//         sx={{
-//           width: "50%",
-//           height: 500,
-//           padding: "100px",
-//           margin: "50px",
-//         }}
-//       >
-//         <Grid container sx={{ justifyContent: "center", height: "100%" }}>
-//           <Grid item md={6} sx={{ height: "100%" }}>
-//             <CardMedia
-//               component="img"
-//               alt="item"
-//               image={item.img}
-//               sx={{
-//                 width: "80%",
-//                 height: "80%",
-//                 objectFit: "contain",
-//               }}
-//             />
-//           </Grid>
-
-//           <Grid item md={6}>
-//             <CardContent
-//               sx={{
-//                 height: 100,
-//                 marginBottom: 30,
-//               }}
-//             >
-//               <Typography
-//                 gutterBottom
-//                 variant="h3"
-//                 component="div"
-//                 sx={{ width: "100%", marginBottom: 10 }}
-//               >
-//                 {item.title}
-//               </Typography>
-
-//               <Typography
-//                 variant="body1"
-//                 color="text.secondary"
-//                 sx={{ marginBottom: 10 }}
-//               >
-//                 {item.description}
-//               </Typography>
-
-//               <Typography
-//                 gutterBottom
-//                 variant="h6"
-//                 component="div"
-//                 sx={{
-//                   width: "100%",
-//                   textAlign: "center",
-//                   height: "10%",
-//                   marginTop: 1,
-//                 }}
-//               >
-//                 Precio: ${item.price}
-//               </Typography>
-//             </CardContent>
-
-//             <CardActions sx={cardActionsStyle}>
-//               <Button onClick={quitar}>
-//                 <AiOutlineMinusSquare size={20} />
-//               </Button>
-//               <Box sx={{ marginLeft: 1 }}>{counter}</Box>
-//               <Button onClick={agregar}>
-//                 <AiOutlinePlusSquare size={20} />
-//               </Button>
-//               <Button onClick={reset}>
-//                 <RxCounterClockwiseClock size={20} />
-//               </Button>
-//             </CardActions>
-//             <CardActions sx={cardActionsStyle}>
-//               <Button sx={{ marginRight: 1 }} size="small" variant="contained">
-//                 Agregar al Carrito
-//               </Button>
-
-//               <Link to={`/`}>
-//                 <Button size="small" variant="contained">
-//                   Volver
-//                 </Button>
-//               </Link>
-//             </CardActions>
-//           </Grid>
-//         </Grid>
-//       </Card>
-//     </div>
