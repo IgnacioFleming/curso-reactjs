5393e1ad02ebe56a0b85d76062b449e00b53365b
Ignacio Fleming <ignacioflemings@gmail.com>
2023-05-12 17:21:09 -0300

    corrijo addToCart y dinamizo CartWidget

--- src/components/Counter/CounterContainer.jsx
+++ src/components/Counter/CounterContainer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Counter from "./Counter";
-import useCounter from "../../utils/hooks/useCounter";
 
 const CounterContainer = ({ stock, onAdd }) => {
   console.log(stock);
